ad80b912726da727e00086044ed3c5624f8446da
limenghua <limenghua@wondersgroup.com>
2018-04-09 17:46:26 +0800

    方法引用

---
+++ src/main/java/com/hua/jdk8/supplier/Car.java
@@ -0,0 +1,25 @@
+package com.hua.jdk8.supplier;
+
+import java.util.function.Supplier;
+
+/**
+ * created at 2018-04-09 17:35
+ * @author limenghua
+ */
+public class Car {
+	public static Car create(Supplier<Car> supplier) {
+		return supplier.get();
+	}
+
+	public static void collide(final Car car) {
+		System.out.println(String.format("Collided %s", car));
+	}
+
+	public void follow(final Car another) {
+		System.out.println(String.format("Following the %s", another));
+	}
+
+	public void repaire() {
+		System.out.println(String.format("Repaired %s", this));
+	}
+}
---
+++ src/main/java/com/hua/jdk8/supplier/SupplierApplication.java
@@ -0,0 +1,29 @@
+package com.hua.jdk8.supplier;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 方法引用使得开发者可以直接引用现存的方法、Java类的构造方法或者实例对象
+ * created at 2018-04-09 17:40
+ * @author limenghua
+ */
+public class SupplierApplication {
+
+	public static void main(String[] args) {
+
+		// 构造器引用
+		final Car car = Car.create(Car::new);
+		final List<Car> cars = Arrays.asList(car);
+
+		// 静态方法引用
+		cars.forEach(Car::collide);
+
+		// 类的成员方法的引用
+		cars.forEach(Car::repaire);
+
+		// 实例对象的成员方法的引用
+		Car police = Car.create(Car::new);
+		cars.forEach(police::follow);
+	}
+}

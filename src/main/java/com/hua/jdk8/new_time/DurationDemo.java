85dd010be84d00663fb8a3def4e23da5134fc0e5
limenghua <limenghua@wondersgroup.com>
2019-07-18 11:15:23 +0800

    计算两个LocalDateTime的间隔-修正秒

--- src/main/java/com/hua/jdk8/new_time/DurationDemo.java
+++ src/main/java/com/hua/jdk8/new_time/DurationDemo.java
@@ -22,11 +22,13 @@ public class DurationDemo {
 	 * <pre>
 	 * 计算间隔秒数
 	 * 如：
-	 * 当前时间：2019-07-16 16:11:16
-	 * 截止时间：2019-07-17 08:00:00
-	 * 间隔秒数：56923863
-	 * 间隔分钟数：948
-	 * 间隔小时数：15
+	 * 当前时间：2019-07-18 11:13:01
+	 * 截止时间：2019-07-19 08:00:00
+	 * 间隔toNanos：74818659000000
+	 * 间隔毫秒数：74818659
+	 * 间隔秒数：74818
+	 * 间隔分钟数：1246
+	 * 间隔小时数：20
 	 * </pre>
 	 * @return
 	 */
@@ -37,9 +39,11 @@ public class DurationDemo {
 		printlnf("截止时间：%s", end);
 		LocalDateTime endTime = LocalDateTime.parse(end, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
 		Duration duration = Duration.between(nowTime, endTime);
-		printlnf("间隔秒数：%s", duration.toMillis());
+		printlnf("间隔toNanos：%s", duration.toNanos());
+		printlnf("间隔毫秒数：%s", duration.toMillis());
+		printlnf("间隔秒数：%s", duration.getSeconds());
 		printlnf("间隔分钟数：%s", duration.toMinutes());
 		printlnf("间隔小时数：%s", duration.toHours());
-		return duration.toMillis();
+		return duration.getSeconds();
 	}
 }
